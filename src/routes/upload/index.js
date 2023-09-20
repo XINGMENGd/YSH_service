41893f9a1e0bc4866b7d0c74b30439c2a3f2b45d
XINGMENGd <379890959@qq.com>
2023-09-20 12:01:01 +0800

    feat:新增上传图片接口

--- src/routes/upload/index.js
+++ src/routes/upload/index.js
@@ -1,7 +1,26 @@
 import express from 'express'
+import multerConfig from '../../utils/multerConfig.js'
 import connection from '../../config/mysql/index.js'
+
+// 上传到服务器地址
+const BaseURL = 'http://localhost:3001' 
+// 上传到服务器的目录
+const imgPath = '/uploads/'
 
 const router = express.Router()
 
+router.post('/upload', (req, res) => {
+  multerConfig.single('file')(req, res, function (err) {
+    if (err) {
+      // 传递的图片格式错误或者超出文件限制大小，就会reject出去
+      console.log(err);
+      res.json(err)
+    } else {
+      // 拼接成完整的服务器静态资源图片路径
+      console.log(BaseURL + imgPath + req.file.filename)
+    }
+  })
+
+})
 
 export default router
